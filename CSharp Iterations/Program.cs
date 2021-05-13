92b5b8971f0fb635764b53d295a4469238064123
Jake <jakenguyen0118@gmail.com>
2021-05-13 16:12:53 -0400

    Add project files.

---
+++ CSharp Iterations/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+
+namespace CSharp_Iterations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //for (var i =1; i <= 10; i++)
+            //{
+            //    if (i % 2 == 0)
+            //    {
+            //        Console.WriteLine(i);
+            //    }
+            //}
+
+            //for (var i = 10; i >= 1; i--)
+            //{
+            //    if (i % 2 == 0)
+            //    {
+            //        Console.WriteLine(i);
+            //    }
+            //}
+
+            //var i = 0;
+            //while (i <= 10)
+            //{
+            //    if (i % 2 == 0)
+            //        Console.WriteLine(i);
+
+            //    i++;
+            //}
+
+            while (true)
+            {
+                Console.Write("Type your name: ");
+                var input = Console.ReadLine();
+
+                if (!String.IsNullOrWhiteSpace(input))
+                {
+                    Console.WriteLine("@Echo: " + input);
+                    // continues to the beginning of the loop
+                    continue;
+                }
+
+                break;
+            }
+
+            //var name = "John Smith";
+
+            //for (var i = 0; i < name.Length; i++)
+            //{
+            //    Console.WriteLine(name[i]);
+            //}
+
+            //foreach (var character in name)
+            //{
+            //    Console.WriteLine(character);
+            //}
+
+            //var numbers = new int[] { 1, 2, 3, 4 };
+
+            //foreach (var number in numbers)
+            //{
+            //    Console.WriteLine(number);
+            //}
+
+
+        }
+    }
+}
